20c6af485d5083c718e471358ab847be4da5b387
shindeyash2304 <shikridat.ys@gmail.com>
2025-07-25 02:29:25 +0530

    Navigation Bar Update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="text-center py-4 mt-10 text-sm text-gray-500">
+      © {new Date().getFullYear()} Job Portal • Made with ❤️ by Yash
+    </footer>
+  );
+};
+
+export default Footer;
